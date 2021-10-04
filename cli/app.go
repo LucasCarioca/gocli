1af4cca78698a6e42217f24c310a6fa7837f6fb1
Lucas Desouza <lucasfds2777@gmail.com>
2021-10-03 21:58:36 -0500

    adding app creator function and default command support

--- cli/app.go
+++ cli/app.go
@@ -1,10 +1,19 @@
 package cli
 
-import "os"
+import (
+	"errors"
+	"os"
+)
 
 type AppInterface interface {
 	AddCommand(name string, command Command)
-	Run()
+	Run() error
+}
+
+func NewApp(defaultCommand Command) AppInterface {
+	app := &App{}
+	app.AddCommand("default", defaultCommand)
+	return app
 }
 
 type App struct {
@@ -15,6 +24,16 @@ func (a *App) AddCommand(name string, command Command) {
 	a.commands[name] = command
 }
 
-func (a *App) Run() {
-	a.commands[os.Args[1]].Run()
+func (a *App) Run() error {
+	cmd, ok := a.commands[os.Args[1]]
+	if ok {
+		return cmd.Run()
+	}
+
+	cmd, ok = a.commands["default"]
+	if ok {
+		return cmd.Run()
+	}
+
+	return errors.New("could not find command")
 }
